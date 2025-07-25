0c81cdaf34f26cd5c5808b754b3fdb171f008fc2
F1dg3t <57208077+F1dg3tXD@users.noreply.github.com>
2025-07-25 09:20:32 -0700

    Complete rebrand

--- globals/globals.gd
+++ globals/globals.gd
@@ -1,6 +1,6 @@
 # your_setup_script.gd
 extends Node
-var map: XRToolsActionMap = preload("res://openxr_action_map.tres")
+#var map: XRToolsActionMap = preload("res://openxr_action_map.tres")
 
 #func _ready():
 	#XRToolsActionMap.set_default_action_map(map)
